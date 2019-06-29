fab76eac40e7c9c22879fb54f5f3abaf57ce4597
daecker <50798284+daecker@users.noreply.github.com>
2019-06-29 09:19:57 -0400

    Delete Text Annotation Code (From Professor Example) From code

--- our_vision.py
+++ our_vision.py
@@ -9,29 +9,15 @@ from google.cloud.vision import types
 
 load_dotenv() # load implicit creds GOOGLE_APPLICATION_CREDENTIALS
 
-def new_client():
-    ##credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILEPATH, AUTH_SCOPE)
-    #credentials = ServiceAccountCredentials._from_parsed_json_keyfile(json.loads(GOOGLE_API_CREDENTIALS), AUTH_SCOPE)
+##SETUP
 
+def new_client():
+    
     client = vision.ImageAnnotatorClient() # todo: explicit credentials
     return client
-
-#def get_text_annotations(img_filepath):
-#    with io.open(img_filepath, 'rb') as image_file:
-#        content = image_file.read()
-#    image = types.Image(content=content) #> <class 'google.cloud.vision_v1.types.Image'>
-#
-#    client = new_client()
-#    response = client.text_detection(image=image) #> <class 'google.cloud.vision_v1.types.AnnotateImageResponse'>
-#    annotations = response.text_annotations #> <class 'google.protobuf.pyext._message.RepeatedCompositeContainer'>
-#    #print("ANNOTATIONS:", len(annotations))
-#    #for annotation in annotations:
-#    #    print("---------------")
-#    #    print(type(annotation), annotation.locale)
-#    #    print(annotation.description)
-#    # each  annotation is a <class 'google.cloud.vision_v1.types.EntityAnnotation'>
-#    descriptions = [a.description.strip() for a in annotations] #> ['1 7 6 3', '1', '7', '6', '3']
-#    return descriptions
+    
+    ##credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILEPATH, AUTH_SCOPE)
+    #credentials = ServiceAccountCredentials._from_parsed_json_keyfile(json.loads(GOOGLE_API_CREDENTIALS), AUTH_SCOPE)
 
 if __name__ == "__main__":
 
